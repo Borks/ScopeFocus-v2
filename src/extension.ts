eba0c829d711740fd7d0e7b20b34ab9d323b4370
Borka Martin Orlov <borka.orlov@fieldforce.com>
2019-09-26 20:47:51 +0300

    Cleared tutorial code.

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import Range from 'vscode';
+
 export function activate(context: vscode.ExtensionContext) {
+	console.info("ScopeFocus loaded");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "scopefocus" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	let activateCommand = vscode.commands.registerCommand('extension.focus', () => {
+		vscode.window.showInformationMessage('Focusing');
 	});
 
-	context.subscriptions.push(disposable);
+	let deactivateCommand = vscode.commands.registerCommand('extension.defocus', () => {
+		vscode.window.showInformationMessage('Defocusing');
+	});
+
+	context.subscriptions.push(activateCommand);
+	context.subscriptions.push(deactivateCommand);
 }
 
-// this method is called when your extension is deactivated
+/**
+ * Apply focus for a specific range
+ *
+ * @param {Range} range Range in active editor to focus on
+ */
+function focusOnRange(range: Range) {
+
+}
+
 export function deactivate() {}
+
