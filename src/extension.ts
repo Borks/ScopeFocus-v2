a7977059a0d7c6cc4bb70ac3b9ba157a80f7f63b
Borka Martin Orlov <borka.orlov@fieldforce.com>
2019-10-05 13:18:54 +0300

    WIP

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,30 @@
-import * as vscode from 'vscode';
+import {
+	ExtensionContext,
+	Position,
+	Range,
+	TextEditor,
+	commands,
+	window
+} from 'vscode';
 
-import { Range } from 'vscode';
-
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 	console.info("ScopeFocus loaded");
 
-	let activateCommand = vscode.commands.registerCommand('extension.focus', () => {
-		vscode.window.showInformationMessage('Focusing');
+	let activateCommand = commands.registerCommand('extension.focus', () => {
+		console.info('Focusing');
 	});
 
-	let deactivateCommand = vscode.commands.registerCommand('extension.defocus', () => {
-		vscode.window.showInformationMessage('Defocusing');
+	let deactivateCommand = commands.registerCommand('extension.defocus', () => {
+		console.info('Defocusing');
 	});
+
+	if (window.activeTextEditor) {
+		let changeWatcher = window.onDidChangeTextEditorSelection( () => {
+			focusOnRange(window.activeTextEditor);
+		});
+
+		context.subscriptions.push(changeWatcher);
+	}
 
 	context.subscriptions.push(activateCommand);
 	context.subscriptions.push(deactivateCommand);
@@ -22,8 +35,16 @@ export function activate(context: vscode.ExtensionContext) {
  *
  * @param {Range} range Range in active editor to focus on
  */
-function focusOnRange(range: Range) {
+function focusOnRange(editor: TextEditor | undefined) {
 
+	// If ---
+	if (editor) {
+		let activeLine = editor.selection.anchor.line;
+		let startRange: Range = new Range(new Position(0,0), new Position(activeLine - 1, 0));
+		let endRange: Range = new Range(new Position(activeLine + 1,0), new Position(600, 0));
+
+		console.log(startRange, endRange);
+	}
 }
 
 export function deactivate() {}
