2ae110a8dae3019e1af8945cbc05425b644826b1
Borka Martin Orlov <borka.orlov@gmail.com>
2019-10-12 21:48:58 +0300

    Focusing on selected blocks of code. WIP

--- src/extension.ts
+++ src/extension.ts
@@ -2,22 +2,28 @@ import {
 	ExtensionContext,
 	Position,
 	Range,
-	TextEditor,
 	TextEditorDecorationType,
 	commands,
-	window,
-	workspace
+	window
 } from 'vscode';
 
 // Decorations to apply to out of focus ranges.
 const OUT_OF_FOCUS_DECORATION = { 'opacity': '0.1' };
 
 
-// Ranges that should be in focus
-const rangesInFocus: Object = {};
+/**
+ * Ranges that should be in focus
+ */
+var rangesInFocus: Range[] = [];
 
-// Ranges by URI that are out of focus
-const rangesOutOfFocus: Object = {};
+
+/**
+ * Ranges that are out of focus
+ */
+var rangesOutOfFocus: Range[] = [];
+
+
+var activeDecorations: TextEditorDecorationType[] = [];
 
 
 export function activate(context: ExtensionContext) {
@@ -25,15 +31,25 @@ export function activate(context: ExtensionContext) {
 
 	let activateCommand = commands.registerCommand('extension.focus', () => {
 		console.info('Focusing');
+		setDecorationRanges();
 	});
 
 	let deactivateCommand = commands.registerCommand('extension.defocus', () => {
 		console.info('Defocusing');
+		rangesInFocus = [];
+		rangesOutOfFocus = [];
 		resetDecorations();
 	});
 
 	let focusSelectionCommand = commands.registerCommand('extension.focusSelection' , () => {
 		console.info('Setting range into focus');
+
+		if (!window.activeTextEditor) { return false; }
+
+		let range: Range = window.activeTextEditor.selection;
+		addRangeToFocus(range);
+		setDecorationRanges();
+		applyDecorations();
 	});
 
 	let defocusSelectionCommand = commands.registerCommand('extension.defocusSelection' , () => {
@@ -47,18 +63,85 @@ export function activate(context: ExtensionContext) {
 }
 
 
+function setDecorationRanges() {
+	if (!window.activeTextEditor) { return false; }
+
+	console.info("Setting decorations");
+
+	let editor = window.activeTextEditor.document;
+
+	let offsets: number[] = [];
+
+	rangesOutOfFocus = [];
+
+	rangesInFocus.map(range => {
+		offsets.push(editor.offsetAt(range.start));
+		offsets.push(editor.offsetAt(range.end));
+	});
+
+	offsets = offsets.sort();
+	console.table(offsets);
+
+	let posA: Position = new Position(0, 0);
+
+	for (let index = 0; index < offsets.length; index = index + 2) {
+		let posB: Position = editor.positionAt(offsets[index]);
+		let outOfFocusRange: Range = new Range(posA, posB);
+		console.log(outOfFocusRange);
+
+		if (index + 1 < offsets.length) {
+			posA = editor.positionAt(offsets[index + 1]);
+		}
+
+		rangesOutOfFocus.push(outOfFocusRange);
+	}
+
+	if (offsets.length > 0) {
+		console.log('hey');
+		let outOfFocusRange: Range = new Range(
+			editor.positionAt(offsets[offsets.length-1]),
+			editor.lineAt(editor.lineCount - 1).range.end
+		);
+
+		rangesOutOfFocus.push(outOfFocusRange);
+	}
+
+	console.log("Ranges out of focus:", rangesOutOfFocus);
+	return rangesOutOfFocus;
+}
+
+
+function addRangeToFocus(range: Range) {
+	if (!window.activeTextEditor) { return false; }
+
+	rangesInFocus.push(range);
+}
+
+function removeRangeFromFocus(position: Position) {
+
+}
+
+
+function applyDecorations() {
+	if (!window.activeTextEditor) { return false; }
+
+	resetDecorations();
+
+	const outOfFocus: TextEditorDecorationType = window.createTextEditorDecorationType(OUT_OF_FOCUS_DECORATION);
+
+	window.activeTextEditor.setDecorations(outOfFocus, rangesOutOfFocus);
+
+	activeDecorations.push(outOfFocus);
+}
+
+
 function resetDecorations() {
+	for (const decoration of activeDecorations) {
+		decoration.dispose();
+	}
+	activeDecorations = [];
 }
 
-
-/**
- * Apply focus for a specific range
- *
- * @param {Range} range Range in active editor to focus on
- */
-function focusOnRange(range: Range) {
-
-}
 
 export function deactivate() {
 	resetDecorations();
