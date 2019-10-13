b62df61f0f89d30b475d9f300fb8a92cbc991eee
Borka Martin Orlov <borka.orlov@gmail.com>
2019-10-13 11:32:19 +0300

    Changed activation event to * + merging focused areas if overlaps

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,9 @@ import {
 	window
 } from 'vscode';
 
-// Decorations to apply to out of focus ranges.
+/**
+ *  Decorations to apply to out of focus ranges.
+ */
 const OUT_OF_FOCUS_DECORATION = { 'opacity': '0.1' };
 
 
@@ -23,6 +25,9 @@ var rangesInFocus: Range[] = [];
 var rangesOutOfFocus: Range[] = [];
 
 
+/**
+ * Decorations currently applied on editors
+ */
 var activeDecorations: TextEditorDecorationType[] = [];
 
 
@@ -42,8 +47,6 @@ export function activate(context: ExtensionContext) {
 	});
 
 	let focusSelectionCommand = commands.registerCommand('extension.focusSelection' , () => {
-		console.info('Setting range into focus');
-
 		if (!window.activeTextEditor) { return false; }
 
 		let range: Range = window.activeTextEditor.selection;
@@ -66,7 +69,39 @@ export function activate(context: ExtensionContext) {
 function setDecorationRanges() {
 	if (!window.activeTextEditor) { return false; }
 
-	console.info("Setting decorations");
+	let editor = window.activeTextEditor.document;
+
+	let offsets = getRangeOffsets();
+	console.table(offsets);
+
+	let posA: Position = new Position(0, 0);
+
+	for (let index = 0; index < offsets.length; index = index + 2) {
+		let posB: Position = editor.positionAt(offsets[index]);
+		let outOfFocusRange: Range = new Range(posA, posB);
+
+		if (index + 1 < offsets.length) {
+			posA = editor.positionAt(offsets[index + 1]);
+		}
+
+		rangesOutOfFocus.push(outOfFocusRange);
+	}
+
+	if (offsets.length > 0) {
+		let outOfFocusRange: Range = new Range(
+			editor.positionAt(offsets[offsets.length-1]),
+			editor.lineAt(editor.lineCount - 1).range.end
+		);
+
+		rangesOutOfFocus.push(outOfFocusRange);
+	}
+
+	return rangesOutOfFocus;
+}
+
+
+function getRangeOffsets() {
+	if (!window.activeTextEditor) { return []; }
 
 	let editor = window.activeTextEditor.document;
 
@@ -80,39 +115,25 @@ function setDecorationRanges() {
 	});
 
 	offsets = offsets.sort();
-	console.table(offsets);
 
-	let posA: Position = new Position(0, 0);
-
-	for (let index = 0; index < offsets.length; index = index + 2) {
-		let posB: Position = editor.positionAt(offsets[index]);
-		let outOfFocusRange: Range = new Range(posA, posB);
-		console.log(outOfFocusRange);
-
-		if (index + 1 < offsets.length) {
-			posA = editor.positionAt(offsets[index + 1]);
-		}
-
-		rangesOutOfFocus.push(outOfFocusRange);
-	}
-
-	if (offsets.length > 0) {
-		console.log('hey');
-		let outOfFocusRange: Range = new Range(
-			editor.positionAt(offsets[offsets.length-1]),
-			editor.lineAt(editor.lineCount - 1).range.end
-		);
-
-		rangesOutOfFocus.push(outOfFocusRange);
-	}
-
-	console.log("Ranges out of focus:", rangesOutOfFocus);
-	return rangesOutOfFocus;
+	return offsets;
 }
 
 
 function addRangeToFocus(range: Range) {
 	if (!window.activeTextEditor) { return false; }
+
+	/**
+	 * Check to see if range can be merged with another
+	 */
+	for (let existingFocus in rangesInFocus) {
+		if (rangesInFocus[existingFocus].intersection(range)) {
+			let newRange: Range = rangesInFocus[existingFocus].union(range);
+			delete rangesInFocus[existingFocus];
+			rangesInFocus.push(newRange);
+			return;
+		}
+	}
 
 	rangesInFocus.push(range);
 }
